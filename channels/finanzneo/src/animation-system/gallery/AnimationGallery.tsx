37d65946d612dc93dea8af089a882f9274e56648
memetreza34-ux <memetreza34@gmail.com>
2026-08-02 16:53:26 +0200

    feat: add isolated finance animation gallery

---
+++ channels/finanzneo/src/animation-system/gallery/AnimationGallery.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {AbsoluteFill, Sequence} from 'remotion';
+import {
+  BeforeAfterComparisonTemplate,
+  BudgetSplitTemplate,
+  CompoundGrowthTemplate,
+  DebtPaydownTemplate,
+  IncomeExpenseBalanceTemplate,
+  InflationErosionTemplate,
+  MoneyFlowTemplate,
+  MonthlyInvestmentTemplate,
+  PortfolioAllocationTemplate,
+  RiskReturnScaleTemplate,
+  TaxFeeFlowTemplate,
+  TimelineMilestonesTemplate,
+} from '../templates';
+
+const CARD_DURATION = 180;
+
+const galleryItems = [
+  <CompoundGrowthTemplate key="compound" startCapital={1000} monthlyRate={200} annualReturn={7} years={20} />,
+  <MoneyFlowTemplate key="flow" amount={300} fromLabel="Gehalt" toLabel="ETF" />,
+  <BudgetSplitTemplate key="budget" income={2500} needsPercent={50} wantsPercent={30} savingsPercent={20} />,
+  <InflationErosionTemplate key="inflation" startingValue={100} inflationPercent={2.5} years={10} />,
+  <PortfolioAllocationTemplate key="portfolio" allocations={[{label: 'Welt-ETF', value: 70}, {label: 'Anleihen', value: 20}, {label: 'Cash', value: 10}]} />,
+  <DebtPaydownTemplate key="debt" originalDebt={12000} remainingDebt={4200} />,
+  <MonthlyInvestmentTemplate key="monthly" monthlyRate={250} months={12} />,
+  <BeforeAfterComparisonTemplate key="before-after" beforeLabel="Nur sparen" afterLabel="Sparen + investieren" beforeValue={12000} afterValue={17800} />,
+  <RiskReturnScaleTemplate key="risk" riskPercent={45} returnPercent={65} />,
+  <TimelineMilestonesTemplate key="timeline" milestones={[{label: 'Start', value: 0}, {label: '5 Jahre', value: 18000}, {label: '10 Jahre', value: 42000}, {label: '20 Jahre', value: 118000}]} />,
+  <IncomeExpenseBalanceTemplate key="income-expense" income={2800} expenses={2100} />,
+  <TaxFeeFlowTemplate key="tax-fee" grossAmount={3000} taxes={620} fees={30} />,
+] as const;
+
+export const FINANCE_ANIMATION_GALLERY_DURATION = galleryItems.length * CARD_DURATION;
+
+export const AnimationGallery: React.FC = () => (
+  <AbsoluteFill style={{backgroundColor: '#06110A'}}>
+    {galleryItems.map((item, index) => (
+      <Sequence
+        key={index}
+        from={index * CARD_DURATION}
+        durationInFrames={CARD_DURATION}
+        name={`Animation ${index + 1}`}
+      >
+        {item}
+      </Sequence>
+    ))}
+  </AbsoluteFill>
+);
