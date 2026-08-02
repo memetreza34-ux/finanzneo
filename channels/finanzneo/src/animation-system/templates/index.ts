ba5c093f4cfa8ee3686563b43766f15f17585883
memetreza34-ux <memetreza34@gmail.com>
2026-08-02 16:48:52 +0200

    feat(animation): export additional finance templates

--- channels/finanzneo/src/animation-system/templates/index.ts
+++ channels/finanzneo/src/animation-system/templates/index.ts
@@ -1,2 +1,6 @@
+export * from './BudgetSplitTemplate';
 export * from './CompoundGrowthTemplate';
+export * from './DebtPaydownTemplate';
+export * from './InflationErosionTemplate';
 export * from './MoneyFlowTemplate';
+export * from './PortfolioAllocationTemplate';
