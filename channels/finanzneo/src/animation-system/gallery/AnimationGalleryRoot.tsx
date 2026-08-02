56f8b1499193b400cf74bbfdb3a8c0df47a5108a
memetreza34-ux <memetreza34@gmail.com>
2026-08-02 18:45:49 +0200

    Register sequential and overview animation galleries

--- channels/finanzneo/src/animation-system/gallery/AnimationGalleryRoot.tsx
+++ channels/finanzneo/src/animation-system/gallery/AnimationGalleryRoot.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {AnimationGallery, FINANCE_ANIMATION_GALLERY_DURATION} from './AnimationGallery';
+import {
+  AnimationGallery,
+  FINANCE_ANIMATION_CARD_DURATION,
+  FINANCE_ANIMATION_GALLERY_DURATION,
+} from './AnimationGallery';
+import {AnimationGalleryOverview} from './AnimationGalleryOverview';
 
 /**
- * Isolierte Galerie-Composition für die vorbereiteten Finanzanimationen.
+ * Isolierte Galerie-Compositions für die vorbereiteten Finanzanimationen.
  * Diese Root-Datei wird absichtlich nicht im produktiven FinanzNeoRoot registriert.
  */
 export const FinanceAnimationGalleryRoot: React.FC = () => (
-  <Composition
-    id="FinanceAnimationGallery"
-    component={AnimationGallery}
-    durationInFrames={FINANCE_ANIMATION_GALLERY_DURATION}
-    fps={30}
-    width={1080}
-    height={1920}
-  />
+  <>
+    <Composition
+      id="FinanceAnimationGallery"
+      component={AnimationGallery}
+      durationInFrames={FINANCE_ANIMATION_GALLERY_DURATION}
+      fps={30}
+      width={1080}
+      height={1920}
+    />
+    <Composition
+      id="FinanceAnimationGalleryOverview"
+      component={AnimationGalleryOverview}
+      durationInFrames={FINANCE_ANIMATION_CARD_DURATION}
+      fps={30}
+      width={2160}
+      height={3840}
+    />
+  </>
 );
