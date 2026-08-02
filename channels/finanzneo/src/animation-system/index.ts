9eeb139d2c676ef6022c07ed7a4eee117043afdf
memetreza34-ux <memetreza34@gmail.com>
2026-08-02 20:18:33 +0200

    Export safe animation ingestion boundary

--- channels/finanzneo/src/animation-system/index.ts
+++ channels/finanzneo/src/animation-system/index.ts
@@ -1,6 +1,7 @@
 export * from './contracts';
 export * from './templateDataContracts';
 export * from './featureFlags';
+export * from './ingestion';
 export * from './router/classifyFinanceScene';
 export * from './router/financeAnimationKeywords';
 export * from './router/financeKeywordMatching';
