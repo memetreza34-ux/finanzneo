1bae318421ea18f95a03b0082270a74533e8efca
memetreza34-ux <memetreza34@gmail.com>
2026-08-02 16:47:22 +0200

    feat(animation): export primitives templates and qa

--- channels/finanzneo/src/animation-system/index.ts
+++ channels/finanzneo/src/animation-system/index.ts
@@ -3,3 +3,6 @@ export * from './featureFlags';
 export * from './router/classifyFinanceScene';
 export * from './calculations/financeMath';
 export * from './templates/registry';
+export * from './templates';
+export * from './primitives';
+export * from './qa/validateAnimationScene';
