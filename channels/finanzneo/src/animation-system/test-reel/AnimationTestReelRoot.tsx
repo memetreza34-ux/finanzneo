0c6dc9ce539bdeaae5735b17b7e2a8c65346dd5c
memetreza34-ux <memetreza34@gmail.com>
2026-08-03 08:22:07 +0200

    Register isolated fallback preview composition

--- channels/finanzneo/src/animation-system/test-reel/AnimationTestReelRoot.tsx
+++ channels/finanzneo/src/animation-system/test-reel/AnimationTestReelRoot.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import {Composition} from 'remotion';
 import {
+  AnimationFallbackPreview,
   AnimationTestReel,
   FINANCE_ANIMATION_TEST_REEL_DURATION,
+  FINANCE_ANIMATION_TEST_SCENE_DURATION,
 } from './AnimationTestReel';
 
 /**
- * Vollständig isolierte Test-Reel-Composition.
- * Sie ist nicht im produktiven FinanzNeoRoot registriert.
+ * Vollständig isolierte Test-Compositions.
+ * Sie sind nicht im produktiven FinanzNeoRoot registriert.
  */
 export const FinanceAnimationTestReelRoot: React.FC = () => (
-  <Composition
-    id="FinanceAnimationTestReel"
-    component={AnimationTestReel}
-    durationInFrames={FINANCE_ANIMATION_TEST_REEL_DURATION}
-    fps={30}
-    width={1080}
-    height={1920}
-  />
+  <>
+    <Composition
+      id="FinanceAnimationTestReel"
+      component={AnimationTestReel}
+      durationInFrames={FINANCE_ANIMATION_TEST_REEL_DURATION}
+      fps={30}
+      width={1080}
+      height={1920}
+    />
+    <Composition
+      id="FinanceAnimationFallbackPreview"
+      component={AnimationFallbackPreview}
+      durationInFrames={FINANCE_ANIMATION_TEST_SCENE_DURATION}
+      fps={30}
+      width={1080}
+      height={1920}
+    />
+  </>
 );
