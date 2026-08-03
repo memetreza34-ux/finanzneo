5ad741c77e6dc0b5debe29149133e617a298ae02
memetreza34-ux <memetreza34@gmail.com>
2026-08-03 17:09:40 +0200

    Export centralized input limits to internal animation tools

--- channels/finanzneo/src/animation-system/internal.ts
+++ channels/finanzneo/src/animation-system/internal.ts
@@ -1,5 +1,6 @@
 export * from './contracts';
 export * from './domainLimits';
+export * from './inputLimits';
 export * from './templateDataContracts';
 export * from './featureFlags';
 export * from './ingestion';
