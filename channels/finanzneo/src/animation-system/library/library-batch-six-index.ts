969b1b554713f0f9e20ce4a0313bf856c5de595b
memetreza34-ux <memetreza34@gmail.com>
2026-08-04 17:20:20 +0200

    Register FinanceNeo batch six root

---
+++ channels/finanzneo/src/animation-system/library/library-batch-six-index.ts
@@ -0,0 +1,4 @@
+import {registerRoot} from 'remotion';
+import {FinanceAnimationLibraryBatchSixRoot} from './FinanceAnimationLibraryBatchSixRoot';
+
+registerRoot(FinanceAnimationLibraryBatchSixRoot);
