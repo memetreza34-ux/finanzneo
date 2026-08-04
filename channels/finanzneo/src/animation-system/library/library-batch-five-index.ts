9785a4f3fdca1939fcaee085c6c2c897aa412bb1
memetreza34-ux <memetreza34@gmail.com>
2026-08-04 14:24:47 +0200

    Register batch five Remotion root

---
+++ channels/finanzneo/src/animation-system/library/library-batch-five-index.ts
@@ -0,0 +1,4 @@
+import {registerRoot} from 'remotion';
+import {FinanceAnimationLibraryBatchFiveRoot} from './FinanceAnimationLibraryBatchFiveRoot';
+
+registerRoot(FinanceAnimationLibraryBatchFiveRoot);
