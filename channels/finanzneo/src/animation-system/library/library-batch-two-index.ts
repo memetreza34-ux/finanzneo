26679f04d32598c498d8c3ade9d60d1cd055537b
memetreza34-ux <memetreza34@gmail.com>
2026-08-04 13:21:34 +0200

    Add second library Remotion entrypoint

---
+++ channels/finanzneo/src/animation-system/library/library-batch-two-index.ts
@@ -0,0 +1,4 @@
+import {registerRoot} from 'remotion';
+import {FinanceAnimationLibraryBatchTwoRoot} from './FinanceAnimationLibraryBatchTwoRoot';
+
+registerRoot(FinanceAnimationLibraryBatchTwoRoot);
