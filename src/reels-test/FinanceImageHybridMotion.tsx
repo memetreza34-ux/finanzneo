d282e58163479476167417bbc973a473f6eaf343
memetreza34-ux <memetreza34@gmail.com>
2026-09-13 06:19:32 +0200

    test: add hybrid composition shell

---
+++ src/reels-test/FinanceImageHybridMotion.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {AbsoluteFill} from 'remotion';
+
+export const FINANCE_IMAGE_HYBRID_SCENE_FRAMES = 150;
+export const FINANCE_IMAGE_HYBRID_FRAMES = FINANCE_IMAGE_HYBRID_SCENE_FRAMES * 3;
+
+export const FinanceImageHybridMotion: React.FC = () => <AbsoluteFill style={{background: '#000'}} />;
