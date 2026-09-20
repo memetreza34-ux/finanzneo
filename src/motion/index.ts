7d72456b8a3e9b3a8041f9eb5588716ca06b4c23
memetreza34-ux <memetreza34@gmail.com>
2026-09-20 07:22:40 +0200

    feat(motion): expose canonical motion API

---
+++ src/motion/index.ts
@@ -0,0 +1,15 @@
+export {FN_MOTION, motionProgress, motionValue, resultProgress} from './tokens';
+export {
+  PremiumPhysicalStage,
+  PhysicalObject,
+  PhysicalBill,
+  PhysicalCoinStack,
+  PhysicalAccount,
+  PhysicalReserveTank,
+  PhysicalCalendarPage,
+  PhysicalWasher,
+} from './physical';
+export {
+  FinanzNeoMotionReferenceV1,
+  FINANZNEO_MOTION_REFERENCE_FRAMES,
+} from './FinanzNeoMotionReferenceV1';
