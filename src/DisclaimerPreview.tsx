6d4a1c76a93ebc6631ba7595a72d05874e39e4ec
memetreza34-ux <memetreza34@gmail.com>
2026-09-25 09:25:21 +0200

    Remove root disclaimer preview after migration

--- src/DisclaimerPreview.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { Disclaimer } from './brand';
-
-// Vorschau des Haftungsausschluss-Intros (9:16). 10s.
-export const DisclaimerPreview: React.FC = () => <Disclaimer durationInSeconds={10} />;
